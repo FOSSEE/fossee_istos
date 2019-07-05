85a9d81638d240f3fa9ed01cb139ef33243b379b
sanjeevi01 <sangv.r99@gmail.com>
2019-07-05 15:30:52 +0530

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+
+function theme_home(){
+	
+	//flexslider
+	jQuery('.flexslider').flexslider({
+    	animation: "slide"	
+    });
+
+}
+
+jQuery(document).ready(function($){
+	theme_home();
+});
+
